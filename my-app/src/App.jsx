2d35ed8682a94073968395a8d6ba7d4cf177560b
phacharapholph <masterth1234@gmail.com>
2025-08-04 18:30:11 +0700

    Portfolio start eiei

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,17 +2,38 @@ import React, { useState } from 'react'
 
 function App() {
 
-    const name = ("Phacharaphol")
-    const [age , setage] = useState (18)
-
   return (
-    <div>
-      <h1>Hello My name is {name}</h1>
-      <h2>My age is {age}</h2>
-      <button onClick={() => setage(age+1)}>add</button>
-      <button onClick={() => setage(age-1)}>low</button>
-      <button onClick={() => setage(18)}>reset</button>
+
+    <div class="bg-neutral-900 min-h-screen">
+
+
+      <div class="sticky top-0 bg-neutral-900 border-b-3 border-cyan-600 p-4 z-50">
+        <h1 className="text-xl font-bold text-center text-white">My Sticky Bar</h1>
+      </div>
+
+
+      <div class="flex flex-col justify-center items-center px-20 py-20">
+        
+        <div class="bg-neutral-800 rounded-4xl w-320 h-180 shadow-2xl mb-20 flex flex-row justify-center items-center">
+          <img class="mr-60 w-auto h-100 border-4 border-cyan-600 rounded-4xl"  src="abc.png" alt="img" />
+          <div class="text-white text-2xl">
+            Name : Phacharaphol Phinitkit <br /><br /> Nickname : Toon <br /><br /> Age : 18 <br /><br /> School : Ratwinit Bangkeao <br /><br /> Hobby : Coding , Listen music
+          </div>
+        </div>
+        
+        <div class="bg-neutral-800 rounded-4xl w-320 h-180 shadow-2xl mb-20">
+        </div>
+        <div class="bg-neutral-800 rounded-4xl w-320 h-180 shadow-2xl mb-20">
+        </div>
+        <div class="bg-neutral-800 rounded-4xl w-320 h-180 shadow-2xl mb-20">
+        </div>
+        <div class="bg-neutral-800 rounded-4xl w-320 h-180 shadow-2xl mb-20">
+        
+        </div>
+      </div>
     </div>
+
+
   )
 }
 
--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/git101/",
+  plugins: [react(), tailwindcss()],
+  theme: {
+    extend: {
+      fontFamily: {
+        jetbrains: ['"JetBrains Mono"', 'monospace'],
+      },
+    },
+  },
 })
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // ครอบคลุม App.jsx และไฟล์อื่นๆใน src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
