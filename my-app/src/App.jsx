d7080c162d8173d4a1c40e59802ea06c770f3b3c
phacharapholph <masterth1234@gmail.com>
2025-08-08 21:09:41 +0700

    add contact + bar

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -7,8 +7,17 @@ function App() {
     <div class="bg-neutral-900 min-h-screen">
 
 
-      <div class="sticky top-0 bg-neutral-900 border-b-3 border-cyan-600 p-4 z-50">
-        <h1 className="text-xl font-bold text-center text-white">My Sticky Bar</h1>
+      <div className="sticky top-0 bg-neutral-900 border-b-3 border-cyan-600 p-4 z-50 flex flex-row justify-between">
+        <div className='flex flex-row justify-around gap-8'>
+          <h1 className="text-white">icon</h1>
+          <h1 className="text-white">Toon</h1>
+        </div>
+        <div className='flex flex-row justify-around gap-8'>
+          <h1 className="text-white">Profile</h1>
+          <h1 className="text-white">Profile</h1>
+          <h1 className="text-white">Profile</h1>
+          <h1 className="text-white">Profile</h1>
+        </div>
       </div>
 
 
@@ -21,9 +30,12 @@ function App() {
           </div>
         </div>
 
+        <div class="bg-neutral-800 rounded-4xl w-320 h-180 shadow-2xl mb-20">
+        </div>
+
         <div class="bg-neutral-900 rounded-4xl w-320 h-180 mb-20 flex flex-col justify-center items-center">
           <div>
-            <h1 class="text-4xl text-white mb-20">My Skills</h1>
+            <h1 class="text-6xl text-white mb-20">My Skills</h1>
           </div>
 
 
@@ -56,8 +68,8 @@ function App() {
                 </div>
               </div>
             </div>
-            
-            
+
+
             <div className="relative group bg-neutral-700 p-4 rounded-xl text-center text-2xl overflow-hidden">
               <img class="w-auto h-30 mx-auto mb-10 mt-5" src="React.png" alt="REACT" />
               <h1>REACT</h1>
@@ -71,8 +83,8 @@ function App() {
                 </div>
               </div>
             </div>
-            
-            
+
+
             <div className="relative group bg-neutral-700 p-4 rounded-xl text-center text-2xl overflow-hidden">
               <img class="w-auto h-30 mx-auto mb-10 mt-5" src="js.png" alt="JAVA SCRIPT" />
               <h1>JAVA SCRIPT</h1>
@@ -86,8 +98,8 @@ function App() {
                 </div>
               </div>
             </div>
-            
-            
+
+
             <div className="relative group bg-neutral-700 p-4 rounded-xl text-center text-2xl overflow-hidden">
               <img class="w-auto h-30 mx-auto mb-10 mt-5" src="C.png" alt="C + +" />
               <h1>C + +</h1>
@@ -101,8 +113,8 @@ function App() {
                 </div>
               </div>
             </div>
-            
-            
+
+
             <div className="relative group bg-neutral-700 p-4 rounded-xl text-center text-2xl overflow-hidden">
               <img class="w-auto h-30 mx-auto mb-10 mt-5" src="python.png" alt="PYTHON" />
               <h1>PYTHON</h1>
@@ -117,15 +129,68 @@ function App() {
               </div>
             </div>
           </div>
+        </div>
+
+        <div class="bg-neutral-900 rounded-4xl w-320 h-240 mb-20 flex flex-col items-center">
+          <h1 class="text-6xl text-white mb-20 mt-15">My Skills</h1>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-10 text-white w-300 h-140 mt-5'>
+
+            <div className='border-cyan-300 border-dashed border-4 rounded-2xl'>
+              <div>
+                <img src="js.png" alt="" />
+                <h1 className='text-3xl ml-3 mr-3'>สวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับ</h1>
+              </div>
+            </div>
+            <div className='border-cyan-300 border-dashed border-4 rounded-2xl'>
+              <div>
+                <img src="js.png" alt="" />
+                <h1 className='text-3xl ml-3 mr-3'>สวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับ</h1>
+              </div>
+            </div>
+            <div className='border-cyan-300 border-dashed border-4 rounded-2xl'>
+              <div>
+                <img src="js.png" alt="" />
+                <h1 className='text-3xl ml-3 mr-3'>สวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับสวัสดีครับ</h1>
+              </div>
+            </div>
+
+          </div>
+
+          <div>
+            <button className='bg-white w-100 h-20 mt-20 cursor-pointer rounded-3xl border-10 border-cyan-300 border-double text-3xl'>More!!</button>
+          </div>
 
         </div>
-        <div class="bg-neutral-800 rounded-4xl w-320 h-180 shadow-2xl mb-20">
-        </div>
-        <div class="bg-neutral-800 rounded-4xl w-320 h-180 shadow-2xl mb-20">
-        </div>
-        <div class="bg-neutral-800 rounded-4xl w-320 h-180 shadow-2xl mb-20">
+
+
+        <div class="bg-neutral-900 rounded-4xl w-320 h-180 mb-20 flex flex-col items-center">
+          <div className='mb-25 mt-15 text-white text-center'>
+            <h1 className='text-6xl'>Contact</h1>
+            <p className='text-3xl mt-5'>contact me!</p>
+          </div>
+
+          <div className='grid grid-cols-1 md:grid-cols-2 text-white w-300 h-140 mt-5'>
+            <div className='flex flex-col items-center h-55'>
+              <h1 className='text-center text-4xl'>Social</h1>
+              <div className='text-left'>
+                <p className='text-xl mt-10'>Facebook : Phacharaphol Phinitkit</p>
+                <p className='text-xl mt-10'>instagram : pc0rqnkz</p>
+              </div>
+            </div>
+            
+            
+            <div className='flex flex-col items-center border-l-2 h-55'>
+              <h1 className='text-center text-4xl'>Social</h1>
+              <div className='text-left'>
+                <p className='text-xl mt-10'>Facebook : Phacharaphol Phinitkit</p>
+                <p className='text-xl mt-10'>instagram : pc0rqnkz</p>
+              </div>
+            </div>
+
+          </div>
 
         </div>
+
       </div>
     </div>
 
